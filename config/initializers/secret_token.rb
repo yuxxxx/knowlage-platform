affe998531909e185c6e8e242612e9d66856a19c
yuuxxxx <yuunnxx@gmail.com>
2013-12-15 11:00:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnowlagePlatform::Application.config.secret_key_base = '92d0e237c505891463fd94018359820c1d95a762cc7dc78469e098275b948a86955f158ec3bf8546e29a10760f5ccf47d536fb7e2bdfeeb3355202971361ccf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnowlagePlatform::Application.config.session_store :cookie_store, key: '_knowlage-platform_session'
